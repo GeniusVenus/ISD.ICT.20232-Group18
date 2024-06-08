db617a8b91ab7b7ac64515f492c518cc18bf240a
Tai Pham <39149250+taipt03@users.noreply.github.com>
2024-06-09 00:29:14 +0700

    fix: pom

--- springboot-backend/src/main/java/com/example/springbootbackend/model/Product.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/model/Product.java
@@ -1,4 +1,4 @@
-package com.example.sprigbootbackend.model;
+package com.example.springbootbackend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
--- springboot-backend/src/main/java/com/example/springbootbackend/service/ProductService.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/service/ProductService.java
@@ -1,6 +1,5 @@
 package com.example.springbootbackend.service;
 
-import com.example.springbootbackend.model.Product;
 
 import java.util.List;
 public interface ProductService {
