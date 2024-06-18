8a038cb4b09484f57f03dd3db9488cc4ec3832c7
Tai Pham <39149250+taipt03@users.noreply.github.com>
2024-06-18 13:14:53 +0700

    update product quantity for order

--- springboot-backend/src/main/java/com/example/springbootbackend/service/ProductService.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/service/ProductService.java
@@ -3,4 +3,6 @@ package com.example.springbootbackend.service;
 
 import java.util.List;
 public interface ProductService {
+    void updateQuantity(int productId, int quantity);
+    void updateQuantitiesForOrder(int orderId);
 }
